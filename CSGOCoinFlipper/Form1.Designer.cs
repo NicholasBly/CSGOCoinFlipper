ee26c337fe21284ed584bed5d7561e4cf8aa27a0
NicholasBly <73457207+NicholasBly@users.noreply.github.com>
2020-10-26 00:52:51 -0400

    Add files via upload

---
+++ CSGOCoinFlipper/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace CSGOCoinFlipper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Sitka Small", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(99, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Flip Coin!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CSGOCoinFlipper.Properties.Resources.terrorist;
+            this.pictureBox1.Location = new System.Drawing.Point(8, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Small", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 322);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Sitka Small", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(99, 299);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Flip 10x";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 359);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CS:GO Coin Flipper";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CSGOCoinFlipper/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSGOCoinFlipper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static Random random = new Random();
+        public double GetRandomNumber(double minimum, double maximum)
+        {
+            return random.NextDouble() * (maximum - minimum) + minimum;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double value = GetRandomNumber(0.0000000000f, 100.0000000000f);
+            if (value <= 50f)
+            {
+                pictureBox1.Image = Properties.Resources.terrorist;
+                pictureBox1.Refresh();
+                pictureBox1.Visible = true;
+                label1.Text = "You Got Terrorists With A Value of: \n" + value;
+                label1.Visible = true;
+            }
+            else if (value >= 50f)
+            {
+                pictureBox1.Image = Properties.Resources.counter_terrorist;
+                pictureBox1.Refresh();
+                pictureBox1.Visible = true;
+                label1.Text = "You Got Counter-Terrorists With A Value of: \n" + value;
+                label1.Visible = true;
+            }
+        }
+        public void outcome()
+        {
+            double value = GetRandomNumber(0.0000000000f, 100.0000000000f);
+            double value1 = GetRandomNumber(0.0000000000f, 100.0000000000f);
+            double value2 = GetRandomNumber(0.0000000000f, 100.0000000000f);
+            double value3 = GetRandomNumber(0.0000000000f, 100.0000000000f);
+            double value4 = GetRandomNumber(0.0000000000f, 100.0000000000f);
+            double value5 = GetRandomNumber(0.0000000000f, 100.0000000000f);
+            double value6 = GetRandomNumber(0.0000000000f, 100.0000000000f);
+            double value7 = GetRandomNumber(0.0000000000f, 100.0000000000f);
+            double value8 = GetRandomNumber(0.0000000000f, 100.0000000000f);
+            double value9 = GetRandomNumber(0.0000000000f, 100.0000000000f);
+            string result = (value + "\n" + value1 + "\n" + value2 + "\n" + value3 + "\n" + value4 + "\n" + value5 + "\n" + value6 + "\n" + value7 + "\n" + value8 + "\n" + value9 + "\n");
+            double divided = ((value + value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9) / 10);
+            if(divided <= 50f)
+            {
+                pictureBox1.Image = Properties.Resources.terrorist;
+                pictureBox1.Refresh();
+                pictureBox1.Visible = true;
+                label1.Text = "You Got Terrorists With A Value of: \n" + divided;
+                label1.Visible = true;
+            }
+            else if(divided >= 50f)
+            {
+                pictureBox1.Image = Properties.Resources.counter_terrorist;
+                pictureBox1.Refresh();
+                pictureBox1.Visible = true;
+                label1.Text = "You Got Counter-Terrorists With A Value of: \n" + divided;
+                label1.Visible = true;
+            }
+            MessageBox.Show("The 10 results are:\n\n" + result + "\nNow, if we divide these by 10, we get:\n\n" + divided);
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            outcome();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            DialogResult Result = MessageBox.Show("Created by icecoldcoke on Steam\n\nWould You Like View My Steam Profile?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (Result == DialogResult.Yes)
+            {
+                System.Diagnostics.Process.Start("http://steamcommunity.com/id/icecoldcoke/");
+            }
+            else if(Result == DialogResult.No)
+            {
+
+            }
+        }
+    }
+}
